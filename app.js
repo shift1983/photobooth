febedb1cf58a1851e84d349ab8f861d15a18cff7
shift1983 <chone@gmx.de>
2026-09-20 19:58:08 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,96 @@
+alert("app.js geladen");
+
+let wakeLock = null;
+
+async function requestWakeLock() {
+
+    try {
+
+        if ("wakeLock" in navigator) {
+
+            wakeLock =
+                await navigator.wakeLock.request("screen");
+
+            console.log("Wake Lock aktiv");
+        }
+
+    } catch (err) {
+
+        console.error(err);
+    }
+}
+
+async function enableFullscreen() {
+
+    try {
+
+        await document.documentElement.requestFullscreen();
+
+    } catch (err) {
+
+        console.error(err);
+    }
+}
+
+document
+    .getElementById("fullscreenBtn")
+    .addEventListener("click", async () => {
+
+        await enableFullscreen();
+
+        await requestWakeLock();
+    });
+
+if ("serviceWorker" in navigator) {
+
+    navigator.serviceWorker
+        .register("service-worker.js")
+        .then(() => {
+
+            console.log("Service Worker aktiv");
+        });
+}
+
+const cameraBtn =
+    document.getElementById("cameraBtn");
+
+const video =
+    document.getElementById("video");
+
+async function startCamera() {
+
+    alert("Secure Context: " + window.isSecureContext);
+
+    alert("mediaDevices vorhanden: " + !!navigator.mediaDevices);
+
+    try {
+
+        const stream =
+            await navigator.mediaDevices.getUserMedia({
+
+                video: {
+                    facingMode: "user"
+                },
+
+                audio: false
+            });
+
+        video.srcObject = stream;
+
+    } catch (error) {
+
+        alert(
+            "Fehler: " +
+            error.name +
+            "\n" +
+            error.message
+        );
+
+        console.error(error);
+    }
+}
+
+cameraBtn.addEventListener(
+    "click",
+    startCamera
+);
---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "photobooth-v1";
+
+const FILES = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
