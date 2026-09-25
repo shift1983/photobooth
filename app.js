4d60d1a5a68600230f6065331d6ae13e4bc640fa
shift1983 <chone@gmx.de>
2026-09-25 10:35:12 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- app.js
+++ app.js
@@ -1181,11 +1181,11 @@ function drawConfettiDecoration(
 
     // vereinzelte Ballons
 const balloons = [
-    { x: 90,  y: 115, color: design.accentColor },
-    { x: 600, y: 95,  color: design.accentColor4 },
-    { x: 1110, y: 125, color: design.accentColor2 },
+    { x: 90,  y: 85, color: design.accentColor },
+    { x: 600, y: 300,  color: design.accentColor4 },
+    { x: 1110, y: 95, color: design.accentColor2 },
     { x: 145, y: 630, color: design.accentColor3 },
-    { x: 1055, y: 610, color: design.accentColor4 }
+    { x: 1055, y: 610, color: design.accentColor5 }
 ];
 
     balloons.forEach(balloon => {
